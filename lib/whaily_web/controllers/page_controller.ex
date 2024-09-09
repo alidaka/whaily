da978482f9d79b767445955e6d1b55e70f8e5d6b
alidaka <augustus.lidaka@calicoenergy.com>
2024-09-08 23:08:46 -0700

    fetching the truck :rocket:

--- lib/whaily_web/controllers/page_controller.ex
+++ lib/whaily_web/controllers/page_controller.ex
@@ -9,43 +9,63 @@ defmodule WhailyWeb.PageController do
     if connected?(socket) do Logger.info "mount connected!!!!" end
     {:ok,
       socket
-      |> assign(others: ["babbys first vra", 64, "third var"])
+      |> assign(others: []) # breadcrumbs in case they become useful later
       |> assign_async(:truck_name, fn -> fetch_truck() end)
       |> assign_async(:temperature, fn -> {:ok, %{temperature: fetch_temp()}} end)}
   end
 
   def fetch_truck do
     Logger.info "fetch_truck let's go"
-    case Finch.build(:get, "https://google.com") |> Finch.request(Whaily.Finch) do
-      {:ok, %Finch.Response{status: 200, body: _body}} ->
-        {:ok, "Tacos and Beer"}
+    #url = "https://clients6.google.com/calendar/v3/calendars/tihhbg3gp215ruuo0nsp3qafgs@group.calendar.google.com/events?calendarId=tihhbg3gp215ruuo0nsp3qafgs%40group.calendar.google.com&singleEvents=true&eventTypes=default&eventTypes=focusTime&eventTypes=outOfOffice&timeZone=America%2FLos_Angeles&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=2024-09-08T00%3A00%3A00-07%3A00&timeMax=2024-10-15T00%3A00%3A00-07%3A00&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs&%24unique=gc237"
+    today = DateTime.now!("America/Los_Angeles")
+    today_iso = DateTime.to_iso8601(today)
+    tomorrow_iso =
+      today
+      |> DateTime.add(1, :day, Tz.TimeZoneDatabase)
+      |> DateTime.to_iso8601
+
+    url = ~s(https://clients6.google.com/calendar/v3/calendars/tihhbg3gp215ruuo0nsp3qafgs@group.calendar.google.com/events?calendarId=tihhbg3gp215ruuo0nsp3qafgs%40group.calendar.google.com&singleEvents=true&eventTypes=default&eventTypes=focusTime&eventTypes=outOfOffice&timeZone=America%2FLos_Angeles&maxAttendees=1&maxResults=250&sanitizeHtml=true&timeMin=#{today_iso}&timeMax=#{tomorrow_iso}&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs&%24unique=gc237)
+
+    case Finch.build(:get, url) |> Finch.request(Whaily.Finch) do
+      {:ok, %Finch.Response{status: 200, body: body}} ->
+        case Jason.decode(body) do
+          {:ok, %{"items" => items}} ->
+            summary = items
+                      |> Enum.map(fn i -> i["summary"] end)
+                      |> Enum.join("; ")
+            {:ok, %{truck_name: summary}}
+
+          {:error, error} ->
+            {:error, "Jason error: #{inspect(error)}"}
+        end
 
       {:ok, %Finch.Response{status: status}} ->
-        {:error, "Failed with status #{status}"}
-
+        {:error, "Request failed with status #{status}"}
       {:error, reason} ->
-        {:error, "Error: #{inspect(reason)}"}
+        {:error, "Request error: #{inspect(reason)}"}
     end
   end
 
   def fetch_temp do
-    10
+    "TBD..."
   end
 
   @impl true
   def render(assigns) do
     ~H"""
-    <div class="container" :if={@temperature.loading}>loading temp...</div>
-    <div class="container" :if={temp = @temperature.ok? && @temperature.result}>Forecast: <%= temp %></div>
-    <div class="container">
-      <.async_result :let={truck} assign={@truck_name}>
-        <:loading>loading truck...</:loading>
-        <:failed :let={failure}>error: <%= inspect failure %></:failed>
-        <%= truck %>
-      </.async_result>
+    <div :if={@temperature.loading}>loading temp...</div>
+    <div :if={temp = @temperature.ok? && @temperature.result}>Forecast: <%= temp %></div>
+    <div>
+      <a href="https://www.chuckshopshop.com/greenwood">
+        <.async_result :let={truck} assign={@truck_name}>
+          <:loading>loading truck...</:loading>
+          <:failed :let={failure}>error: <%= inspect failure %></:failed>
+          <%= truck %>
+        </.async_result>
+      </a>
     </div>
     <%= for item <- @others do %>
-      <div class="container">
+      <div>
         hello "<%= item %>"
       </div>
     <% end %>
