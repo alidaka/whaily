3df2438eff24499515236c67e81502fc52f90d98
alidaka <augustus.lidaka@calicoenergy.com>
2025-09-02 22:53:36 -0700

    pwa: add manifest.json, icons

--- lib/whaily_web.ex
+++ lib/whaily_web.ex
@@ -17,7 +17,7 @@ defmodule WhailyWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts icons favicon.ico robots.txt manifest.json)
 
   def router do
     quote do
